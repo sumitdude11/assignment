3680e9b364e660cf19119d86fe9d19def0a05eca
sumit rathore <sumitrathore1108@gmail.com>
2021-05-09 23:55:37 +0530

    Update Sorting using fork().c

--- Sorting using fork().c
+++ Sorting using fork().c
@@ -1,7 +1,7 @@
 #include<stdio.h> 
 #include<stdlib.h> 
 #include<sys/wait.h> //for waitpid
-#include<unistd.h>//provide access to posix os api
+#include<unistd.h>//provide access to posix os api  fullform unix standard
 
 int partition(int a[], int p ,int r)
 {
